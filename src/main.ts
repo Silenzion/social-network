7cc8781add343c34f0b543491da9d8889ef2b435
Svetlana Dmitrieva <53534598+Silenzion@users.noreply.github.com>
2022-05-31 15:02:54 +0300

    Updated package list. Added Element Plus.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "@/assets/styles/index.scss";
 import App from "@/App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
 const app = createApp(App);
+
+app.use(ElementPlus);
 app.use(createPinia());
 app.mount("#app");
