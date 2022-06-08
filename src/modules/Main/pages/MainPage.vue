dd00ef6871661ab2771f1c68557cb7f54c3072fe
Svetlana Dmitrieva <53534598+Silenzion@users.noreply.github.com>
2022-06-08 18:03:46 +0300

    Updated main page

--- src/modules/Main/pages/MainPage.vue
+++ src/modules/Main/pages/MainPage.vue
@@ -1,18 +1,19 @@
 <template>
   <el-card class="overflow-y-visible p-[20px]">
     <div class="flex flex-col lg:flex-row">
-    <div class="grow bg-blue-600 py-[30px]">
-      <css-doodle class="grow h-[400px] mx-auto w-[80%] max-w-[] " grid="1x18">
+    <div class="grow py-[30px]">
+      <css-doodle class="grow h-[400px] mx-auto w-[80%] max-w-[] " grid="1x18" click-to-update>
           :doodle {
            max-height: 60vw;
           }
+
           @place-cell: center;
           @size: calc(@i()*6%);
           border-radius: 50%;
           border-style: dashed;
           border-width: calc(@i() * 2px);
           border-color: hsla(
-          @rand(150, 180), 100%, 50%,
+          @rand(210, 240), 100%, 10%,
           calc(3 / @i() * .8)
           );
           @keyframes changeRotate {
@@ -29,12 +30,14 @@
           animation-direction: alternate;
           animation-delay: calc(0.08s * @i());
           @at(1, 13) {
+        transition: @r(.3s) ease;
           border-style: dotted;
           border-color: hsla(
           190, 70%, 50%, 90%
           );
           }
           @at(1, 17) {
+        transition: @r(.3s) ease;
           border-style: dotted;
           border-color: hsla(
           190, 70%, 50%, 90%
@@ -42,7 +45,7 @@
           }
       </css-doodle>
     </div>
-      <div class="w-full max-w-[55%] px-[32px]">
+      <div class="w-full px-[32px] lg:max-w-[55%]">
         <h1 class="text-xl font-medium">Hello!</h1>
         <div class="py-[32px] text-lg">
           Dear friend, welcome to our social network! Here you can find a lot of interesting content and new friends! Rather join
