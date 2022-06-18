f8769c131366f9683c30774810a3e3f131323548
Svetlana Dmitrieva <53534598+Silenzion@users.noreply.github.com>
2022-06-18 09:29:40 +0300

    Updated auth components, removed vuelidate

--- src/modules/User/models/LoginData.ts
+++ src/modules/User/models/LoginData.ts
@@ -1,4 +1,5 @@
-import { required, email } from "@vuelidate/validators";
+import { FormRules } from "element-plus";
+import { reactive } from "vue";
 
 export default class LoginData {
   public constructor(init?: Partial<LoginData>) {
@@ -9,9 +10,7 @@ export default class LoginData {
   public password = "";
 }
 
-export const validationsRule = () => {
-  return {
-    email: { required, email },
-    password: { required },
-  };
-};
+export const validationsRule = reactive<FormRules>({
+  email: [{ required: true, type: "email", message: "Please enter correct email!", trigger: ["blur", "change"] }],
+  password: [{ required: true, min: 6, message: "Passwords must have at least 6 symbols!", trigger: ["blur", "change"] }],
+});
--- src/modules/User/pages/LoginForm.vue
+++ src/modules/User/pages/LoginForm.vue
@@ -1,45 +1,52 @@
 <script lang="ts" setup>
 import useVuelidate from "@vuelidate/core";
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import { useRouter } from "vue-router";
 import ServiceManager from "@/_core/ServiceManager";
 import AuthService from "@/modules/User/AuthService";
 import LoginData, { validationsRule } from "@/modules/User/models/LoginData";
 import { AuthResult, AuthStatusType } from "@/modules/User/models/AuthResult";
+import { FormInstance } from "element-plus";
 
 const router = useRouter();
 const internalModel = reactive(new LoginData());
-const v$ = useVuelidate(validationsRule(), internalModel);
-
-const sendForm = () => {
-  v$.value.$touch();
-  if (v$.value.$invalid) {
-    return;
-  }
-  const response: AuthResult = ServiceManager.getInstance().getService(AuthService).login(internalModel);
-
-  if (response.status == AuthStatusType.OK) {
-    router.push({
-      name: "user-page",
-      params: {
-        id: ServiceManager.getInstance().getService(AuthService).getCurrentUser().id,
-      },
-    });
-  } else {
-    console.log(response);
-  }
+const authError = ref(false);
+const loginForm = ref<FormInstance>();
+const sendForm = async () => {
+  // const response: AuthResult = ServiceManager.getInstance().getService(AuthService).login(internalModel);
+  if (!loginForm.value) return;
+  await loginForm.value.validate((valid, fields) => {
+    if (valid) {
+      console.log("submit!");
+    } else {
+      console.log("error submit!", fields);
+    }
+  });
+  // if (response.status == AuthStatusType.OK) {
+  //   router.push({
+  //     name: "user-page",
+  //     params: {
+  //       id: ServiceManager.getInstance().getService(AuthService).getCurrentUser().id,
+  //     },
+  //   });
+  // } else {
+  //   authError.value = true;
+  //   console.log(response);
+  // }
 };
 </script>
 <template>
-  <main class="flex h-full items-center justify-center">
-    <el-card>
-      <el-form :rules="validationsRule()">
-        <el-form-item label="Please input email">
-          <el-input v-model="internalModel.email" @blur="v$.phone.$touch()"></el-input>
+  <main class="flex min-h-[100vh] items-center justify-center">
+    <el-card class="w-full max-w-[400px]">
+      <div class="mb-[32px] text-center text-xl">Log in</div>
+      <el-form ref="loginForm" :rules="validationsRule" :model="internalModel" status-icon>
+        <el-form-item label="Email" prop="email">
+          <el-input v-model="internalModel.email"></el-input>
         </el-form-item>
-        <el-form-item label="Please input password">
-          <el-input v-model="internalModel.password" @blur="v$.phone.$touch()" />
+        <el-form-item label="Password" prop="password">
+          <el-input v-model="internalModel.password" type="password" />
         </el-form-item>
+        <div v-if="authError" class="text-red-700">Sorry, user with the credentials not found!</div>
         <el-button type="primary" @click="sendForm()">Log in</el-button>
       </el-form>
     </el-card>
